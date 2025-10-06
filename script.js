fa848c4086ed3e4e550b552582ff414390c2540c
Heliton07 <148549955+Heliton07@users.noreply.github.com>
2025-10-06 07:23:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,91 +1,160 @@
-const caixaPrincipal = documento.querySelector(".caixa-principal")
-const caixaPerguntas = documento.querySelector(".caixa-perguntas")
-const caixaAlternativas = documento.querySelector(".caixa-alternativas")
-const caixaResultados = documento.querySelector(".caixa-resultados")
-const textoResultado = documento.querySelector(".texto-resultados")
+
+const caixaPrincipal = document.querySelector(".caixa-principal")
+const caixaPerguntas = document.querySelector(".caixa-perguntas")
+const caixaAlternativas = document.querySelector(".caixa-alternativas")
+const caixaResultado = document.querySelector(".caixa-resultado")
+const textoResultado = document.querySelector(".texto-resultado")
+
+
+
 
 const perguntas = [
-{
-enunciado:"pergunta 1",
-alternativas:[
-    {
-        texto: "alternativa 1",
-        afirmacao: "afirmacao"
-    },
-     {
-        texto: "alternativa 2",
-        afirmacao: "afirmacao"
-     }
-    
-    ]
-},   
-{    
-enunciado: "pergunta 2",
-alternativas: [
-    {
-        texto: "alternativa 1",
-        afirmacao: "afirmacao"
-    },
-     {
-        texto: "alternativa 2",
-        afirmacao: "afirmacao"
-     }
-    ]
-},
-{    
-    enunciado:"pergunta 3",
-    alternativas:[{
-        texto: "alternativa 1",
-        afirmacao: "afirmacao"
-    },
-     {
-        texto: "alternativa 2",
-        afirmacao: "afirmacao"
-     }]
-},
-{
-    enunciado:"pergunta 4",
-    alternativas:[{
-        texto: "alternativa 1",
-        afirmacao: "afirmacao"
-    },
-     {
-        texto: "alternativa 2",
-        afirmacao: "afirmacao"
-     }]
-},
-{    
-   enunciado:"pergunta 5",
-   alternativas:[{
-    texto: "alternativa 1",
-    afirmacao: "afirmacao"
-},
- {
-    texto: "alternativa 2",
-    afirmacao: "afirmacao"
- }]
-},               
+   {
+     enunciado: "Pergunta 1",
+      alternativas: [
+        {
+            texto: "Alternativa 1",
+            afirmacao: "afirmacao"
+        },
+        {
 
+
+
+
+           texto: "Alternativa 2",
+           afirmacao: "afirmacao"}
+        ]
+       
+
+
+
+
+    },
+    {
+         enunciado: "Pergunta 2",
+         alternativas: [
+        {
+            texto: "Alternativa 1",
+            afirmacao: "afirmacao"
+        },
+        {
+
+
+
+
+           texto: "Alternativa 2",
+           afirmacao: "afirmacao"}
+        ]
+
+
+
+
+    },
+    {
+         enunciado: "Pergunta 3",
+         alternativas: [
+        {
+            texto: "Alternativa 1",
+            afirmacao: "afirmacao"
+        },
+        {
+
+
+
+
+           texto: "Alternativa 2",
+           afirmacao: "afirmacao"}
+        ]
+    },
+    {
+        enunciado: "Pergunta 4",
+        alternativas: [
+        {
+            texto: "Alternativa 1",
+            afirmacao: "afirmacao"
+        },
+        {
+
+
+
+
+           texto: "Alternativa 2",
+           afirmacao: "afirmacao"}
+        ]
+    },
+    {
+        enunciado: "Pergunta 5",
+        alternativas: [
+        {
+            texto: "Alternativa 1",
+            afirmacao: "afirmacao"
+        },
+        {
+
+
+
+
+           texto: "Alternativa 2",
+           afirmacao: "a:root{
+            --cor-fundo:#;
+            --cor-principal:#;
+            --cor-secundaria:#;
+            --cor-destaque:#;
+            --cor-texto:#;
+            }firmacao"}
+        ]
+    }
 ];
+
+
+
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
-function mostrarperguntas(){
- perguntaAtual = pergunta[atual];  
- caixaPerguntas. textcontent = perguntaAtual. enunciado;
- mostrarAlternativas();
-}
-function mostrarAlternativas(){
- for(const alternativa of perguntaAtual.alternativas){
-const botaoAlternativas = document.createElement(button)
-botaoAlternativas.textcontent = alternativa.texto;
-botaoAlternativa.addEventlistner("click", function(){
-atual++;
-mostrarpergunta();
-})
-caixaAlternativas.appendChild(botaoAlternativas);
- }   
+
+function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
 }
 
-mostrarperguntas();
+
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+}
+
+
+function mostraResultado() {
+    caixaPerguntas.textContent = " ";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
+
+
+mostraPergunta();:root{
+    --cor-fundo:#;
+    --cor-principal:#;
+    --cor-secundaria:#;
+    --cor-destaque:#;
+    --cor-texto:#;
+    }
